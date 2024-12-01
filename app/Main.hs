06039f6435f0ce45bf12ef6899cabb04ce33b1e6
Javier R <javier.rios-urbano@epitech.eu>
2024-12-01 14:35:23 +0100

    add: simple `codegen` example

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,18 @@
 module Main where
 
+import qualified Ast.Types as T
+import qualified Codegen.Codegen as C
+import qualified Data.Text.Lazy as TL
+import qualified LLVM.Pretty as P
+
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  let ast =
+        T.Expr
+          ( T.If
+              (T.Lit (T.LInt 1))
+              (T.Op T.Add (T.Lit (T.LInt 2)) (T.Lit (T.LInt 3)))
+              (T.Op T.Multiply (T.Lit (T.LInt 4)) (T.Lit (T.LInt 5)))
+          )
+  let output = TL.unpack $ P.ppllvm $ C.codegen ast
+  writeFile "demo/generated.ll" output
