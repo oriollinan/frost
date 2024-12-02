9990e675a5e593c3f5bd9e164fbb0046988018fa
Oriol Linan <oriol.linan@epitech.eu>
2024-12-02 11:49:52 +0100

    mod: `Ast` type

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,11 @@ import qualified LLVM.Pretty as P
 main :: IO ()
 main = do
   let ast =
-        T.Expr
-          ( T.If
+        T.AST
+          [ T.If
               (T.Lit (T.LInt 1))
               (T.Op T.Add (T.Lit (T.LInt 2)) (T.Lit (T.LInt 3)))
-              (T.Op T.Multiply (T.Lit (T.LInt 4)) (T.Lit (T.LInt 5)))
-          )
+              (T.Op T.Mult (T.Lit (T.LInt 4)) (T.Lit (T.LInt 5)))
+          ]
   let output = TL.unpack $ P.ppllvm $ C.codegen ast
   writeFile "demo/generated.ll" output
