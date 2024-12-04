6c87a9aa108937a275619752b22e3cc505c561f0
Javier R <javier.rios-urbano@epitech.eu>
2024-12-04 13:21:41 +0100

    add: new `ast` program with `global` variables

--- demo/add.lisp
+++ demo/add.lisp
@@ -1,10 +1,11 @@
-; Sample LISP code, evaluates to `3`
-(define lambda_0 (lambda (x) (+ x 1)))
+; Sample LISP code, evaluates to `42`
+(define example 21)
+(define lambda_0 (lambda (x) (* x 2)))
 (define lambda_1 (lambda (y) (- y 1)))
 
 (define $$generated
     (if (= 1 1)
-        (lambda_0 2)
+        (lambda_0 example)
         (lambda_1 2)))
 
 ($$generated)
