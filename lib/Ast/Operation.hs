49310fb3294d8c1bf75bfcf2f6831694e6acc38e
Oriol Linan <oriol.linan@epitech.eu>
2024-12-18 12:28:39 +0100

    add: `Operation`

---
+++ lib/Ast/Operation.hs
@@ -0,0 +1,36 @@
+module Ast.Operation where
+
+import qualified Ast.Types as AT
+import qualified Ast.Utils as AU
+import qualified Text.Megaparsec as M
+
+-- | Operator symbols mapped to their AST representation.
+operations :: [(String, AT.Operation)]
+operations =
+  [ ("+", AT.Add),
+    ("-", AT.Sub),
+    ("*", AT.Mul),
+    ("/", AT.Div),
+    ("mod", AT.Mod),
+    ("&&", AT.And),
+    ("and", AT.And),
+    ("||", AT.And),
+    ("or", AT.And),
+    ("&", AT.BitAnd),
+    ("|", AT.BitOr),
+    ("^", AT.BitXor),
+    ("<<", AT.BitShl),
+    (">>", AT.BitShr),
+    ("<=", AT.Lte),
+    (">=", AT.Gte),
+    ("<", AT.Lt),
+    (">", AT.Gt),
+    ("==", AT.Eq),
+    ("is", AT.Eq),
+    ("!=", AT.Ne)
+  ]
+
+-- | Parses a symbol into an `AT.Operation` using the `operations` list.
+-- Returns the corresponding `AT.Operation` if a match is found.
+parseOperation :: AU.Parser AT.Operation
+parseOperation = M.choice $ (\(o, c) -> c <$ AU.symbol o) <$> operations
