ad4ff32688a96a5ccc41c90a16197abf67fb7886
Renzo Maggiori <maggiorirenzo@gmail.com>
2024-12-30 14:41:21 +0100

    [add] `Type Parser` functions, structs, unions and typedefs

--- lib/Ast/Parser/Type.hs
+++ lib/Ast/Parser/Type.hs
@@ -8,9 +8,13 @@ import qualified Text.Megaparsec as M
 import qualified Text.Megaparsec.Char as MC
 import qualified Text.Megaparsec.Char.Lexer as ML
 
+-- | Parse a general type. This function combines multiple specific type parsers.
+-- It tries to match typedefs, structs, unions, functions, mutable types, pointers, and base types.
 parseType :: AU.Parser AT.Type
-parseType = AU.triedChoice [structType, mutableType, typedefType, pointerType, arrayType, baseType]
+parseType = AU.triedChoice [structType, unionType, typedefType, functionType, mutableType, pointerType, baseType]
 
+-- | A list of predefined base types along with their associated keywords.
+-- These include basic types such as int, float, double, char, bool, and void.
 baseTypes :: [(String, AT.Type)]
 baseTypes =
   [ ("int", AT.TInt 0),
@@ -21,41 +25,72 @@ baseTypes =
     ("void", AT.TVoid)
   ]
 
--- | Parses a base type using the `baseTypes` list.
+-- | Parses a base type by matching one of the predefined base type keywords.
+-- Example: "int" or "bool".
 baseType :: AU.Parser AT.Type
 baseType = M.choice $ (\(kw, ty) -> ty <$ AU.symbol kw) <$> baseTypes
 
--- | Parse a pointer type (e.g., `*int`)
+-- | Parses a pointer type.
+-- A pointer type is denoted by a '*' followed by another type.
+-- Example: "*int" results in a pointer to an integer.
 pointerType :: AU.Parser AT.Type
 pointerType = AT.TPointer <$> (MC.char '*' *> parseType)
 
+-- | Parses a mutable type.
+-- A mutable type is prefixed by the keyword "mutable" followed by the type.
+-- Example: "mutable int" indicates a mutable integer type.
 mutableType :: AU.Parser AT.Type
 mutableType = AT.TMutable <$> (MC.string "mutable" *> AU.sc *> parseType)
 
--- ! fix not working properly, infinite loop because of `parseType`
-arrayType :: AU.Parser AT.Type
-arrayType = do
-  t <- parseType
-  size <- M.optional $ M.between (AU.symbol "[") (AU.symbol "]") ML.decimal
-  pure (AT.TArray t size)
+-- | Parses a struct type definition.
+-- A struct is defined with the "struct" keyword followed by an optional name and a list of fields enclosed in braces.
+-- Example: "struct { x -> int, y -> float }".
+structType :: AU.Parser AT.Type
+structType = do
+  name <- identifier
+  _ <- AU.symbol "::" <* AU.symbol "struct"
+  fields <- M.between (MC.char '{') (MC.char '}') $ M.many (AU.sc *> parseField)
+  return $ AT.TStruct {AT.structName = name, AT.fields = fields}
 
--- ! fix not working properly, infinite loop because of `parseType`
+-- | Parses a union type definition.
+-- A union is defined with the "union" keyword followed by an optional name and a list of variants enclosed in braces.
+-- Example: "union { data -> *char, error -> int }".
+unionType :: AU.Parser AT.Type
+unionType = do
+  name <- identifier
+  _ <- AU.symbol "::" <* AU.symbol "union"
+  variants <- M.between (MC.char '{') (MC.char '}') $ M.many (AU.sc *> parseField)
+  return $ AT.TUnion {AT.unionName = name, AT.variants = variants}
+
+-- | Parses a typedef.
+-- A typedef associates a new name with an existing type using the "::" syntax.
+-- Example: "Vector2i :: struct { x -> int, y -> int }".
+typedefType :: AU.Parser AT.Type
+typedefType = do
+  name <- identifier
+  _ <- AU.symbol "::"
+  AT.TTypedef name <$> parseType
+
+-- | Parses a function type.
+-- A function type is defined by its parameter types separated by spaces, followed by "->" and the return type.
+-- Example: ": Vector2i -> Response" or ": int float -> double".
 functionType :: AU.Parser AT.Type
 functionType = do
-  paramTypes <- M.some parseType
+  _ <- AU.symbol ":"
+  paramTypes <- M.some (parseType <* AU.sc)
   _ <- AU.symbol "->"
   returnType <- parseType
-  pure (AT.TFunction returnType paramTypes False)
-
--- TODO: after having a decided syntax for them implement `struct`, `union` and `typedef`
-structType :: AU.Parser AT.Type
-structType = pure (AT.TStruct "placeholder" [("field", AT.TInt 0)])
-
-unionType :: AU.Parser AT.Type
-unionType = pure (AT.TUnion "placeholder" [("variant", AT.TFloat)])
-
-typedefType :: AU.Parser AT.Type
-typedefType = pure (AT.TTypedef "placeholder" AT.TChar)
+  return $ AT.TFunction {AT.returnType = returnType, AT.paramTypes = paramTypes, AT.isVariadic = False}
 
 identifier :: AU.Parser String
-identifier = AU.lexeme ((:) <$> MC.letterChar <*> M.many MC.letterChar)
+identifier = AU.lexeme ((:) <$> MC.letterChar <*> M.many MC.alphaNumChar)
+
+-- | Parses a single field within a struct or union.
+-- Each field consists of a name followed by "->" and its type.
+-- Example: "x -> int".
+parseField :: AU.Parser (String, AT.Type)
+parseField = do
+  fieldName <- identifier
+  _ <- AU.symbol "->"
+  fieldType <- parseType
+  return (fieldName, fieldType)
--- test/Ast/Parser/TypeSpec.hs
+++ test/Ast/Parser/TypeSpec.hs
@@ -37,27 +37,27 @@ spec = do
     it "parses mutable int" $ do
       parse P.parseType "" "mutable int" `shouldBe` Right (AT.TMutable (AT.TInt 0))
 
-  describe "Array Types" $ do
-    it "parses int[10]" $ do
-      parse P.parseType "" "int[10]" `shouldBe` Right (AT.TArray (AT.TInt 0) (Just 10))
-
-    it "parses int[]" $ do
-      parse P.parseType "" "int[]" `shouldBe` Right (AT.TArray (AT.TInt 0) Nothing)
-
   describe "Function Types" $ do
     it "parses int -> float" $ do
-      parse P.parseType "" "int -> float" `shouldBe` Right (AT.TFunction AT.TFloat [AT.TInt 0] False)
+      parse P.parseType "" ": int -> float" `shouldBe` Right (AT.TFunction {AT.returnType = AT.TFloat, AT.paramTypes = [AT.TInt 0], AT.isVariadic = False})
 
     it "parses int int -> void" $ do
-      parse P.parseType "" "int int -> void" `shouldBe` Right (AT.TFunction AT.TVoid [AT.TInt 0, AT.TInt 0] False)
+      parse P.parseType "" ": int int -> void" `shouldBe` Right (AT.TFunction {AT.returnType = AT.TVoid, AT.paramTypes = [AT.TInt 0, AT.TInt 0], AT.isVariadic = False})
 
   describe "Struct Types" $ do
     it "parses struct { name -> int }" $ do
-      parse P.parseType "" "test :: struct { name -> int }" `shouldBe` Right (AT.TStruct "test" [("name", AT.TInt 0)])
+      parse P.parseType "" "test :: struct { name -> int }" `shouldBe` Right (AT.TStruct {AT.structName = "test", AT.fields = [("name", AT.TInt 0)]})
 
     it "parses struct with multiple fields" $ do
-      parse P.parseType "" "vec :: struct { x -> float, y -> float }" `shouldBe` Right (AT.TStruct "vec" [("x", AT.TFloat), ("y", AT.TFloat)])
+      parse P.parseType "" "vec :: struct { x -> float y -> float }" `shouldBe` Right (AT.TStruct {AT.structName = "vec", AT.fields = [("x", AT.TFloat), ("y", AT.TFloat)]})
 
   describe "Union Types" $ do
-    it "parses union { success -> int, error -> *char }" $ do
-      parse P.parseType "" "response :: union { success -> int, error -> *char }" `shouldBe` Right (AT.TUnion "response" [("success", AT.TInt 0), ("error", AT.TPointer AT.TChar)])
+    it "parses union { name -> int }" $ do
+      parse P.parseType "" "test :: union { name -> int }" `shouldBe` Right (AT.TUnion {AT.unionName = "test", AT.variants = [("name", AT.TInt 0)]})
+
+    it "parses union with multiple fields" $ do
+      parse P.parseType "" "vec :: union { x -> float y -> float }" `shouldBe` Right (AT.TUnion {AT.unionName = "vec", AT.variants = [("x", AT.TFloat), ("y", AT.TFloat)]})
+
+  describe "Typedefs" $ do
+    it "parses typedef alias" $ do
+      parse P.parseType "" "Alias :: int" `shouldBe` Right (AT.TTypedef "Alias" (AT.TInt 0))
