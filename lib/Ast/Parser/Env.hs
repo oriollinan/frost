5c6dc786734e40dfc8a088f66fc61b6dc5b31595
Oriol Linan <oriol.linan@epitech.eu>
2025-01-06 21:43:00 +0100

    mod: `Env` to use record syntax

--- lib/Ast/Parser/Env.hs
+++ lib/Ast/Parser/Env.hs
@@ -3,7 +3,9 @@ module Ast.Parser.Env where
 import qualified Ast.Types as AT
 
 -- | The environment for storing custom type definitions.
-newtype Env = Env [(String, AT.Type)]
+newtype Env = Env
+  { types :: [(String, AT.Type)]
+  }
 
 -- | Creates an empty environment.
 emptyEnv :: Env
@@ -12,8 +14,8 @@ emptyEnv = Env []
 -- | Inserts a custom type into the environment.
 -- If the type already exists, it overwrites it.
 insertType :: String -> AT.Type -> Env -> Env
-insertType name ty (Env env) = Env ((name, ty) : env)
+insertType name t env = env {types = (name, t) : types env}
 
 -- | Looks up a custom type in the environment by its name.
 lookupType :: String -> Env -> Maybe AT.Type
-lookupType name (Env env) = lookup name env
+lookupType name env = lookup name $ types env
