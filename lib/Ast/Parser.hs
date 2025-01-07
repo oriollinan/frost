2658f0fa9437eb767c2f7a93c6a970e2508dc326
Oriol Linan <oriol.linan@epitech.eu>
2025-01-07 12:25:36 +0100

    add: `parse`

--- lib/Ast/Parser.hs
+++ lib/Ast/Parser.hs
@@ -1,7 +1,7 @@
 module Ast.Parser where
 
 import qualified Ast.Parser.Env as E
-import qualified Ast.Parser.Utils as PU
+import qualified Ast.Parser.Program as PP
 import qualified Ast.Types as AT
 import qualified Control.Monad.State as S
 import qualified Text.Megaparsec as M
@@ -9,12 +9,7 @@ import qualified Text.Megaparsec as M
 -- | Parses a string into an abstract syntax tree (AST).
 -- The `parse` function takes a filename `String`, and an input `String` as a parameter and returns either an AST or an error message.
 parse :: String -> String -> Either String AT.Program
-parse filename input =
-  case S.runState (M.runParserT parseProgram filename input) E.emptyEnv of
+parse sourceFile input =
+  case S.runState (M.runParserT (PP.parseProgram sourceFile) sourceFile input) E.emptyEnv of
     (Left err, _) -> Left (M.errorBundlePretty err)
     (Right program, _) -> Right program
-
--- | Parses the top-level structure of a program.
--- Returns an `AST` object containing a list of parsed expressions.
-parseProgram :: PU.Parser AT.Program
-parseProgram = fail "undefined"
