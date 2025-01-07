ef6936c7e934804fd900788c5899d7d0d71d63d2
Oriol Linan <oriol.linan@epitech.eu>
2025-01-07 10:15:58 +0100

    add: parse `Declaration`

--- lib/Ast/Parser/Expr.hs
+++ lib/Ast/Parser/Expr.hs
@@ -2,6 +2,7 @@ module Ast.Parser.Expr where
 
 import qualified Ast.Parser.Env as E
 import qualified Ast.Parser.Literal as PL
+import qualified Ast.Parser.Type as PT
 import qualified Ast.Parser.Utils as PU
 import qualified Ast.Types as AT
 import qualified Control.Monad.State as S
@@ -9,7 +10,7 @@ import qualified Text.Megaparsec as M
 import qualified Text.Megaparsec.Pos as MP
 
 parseExpr :: PU.Parser AT.Expr
-parseExpr = PU.triedChoice [parseLit]
+parseExpr = PU.triedChoice [parseLit, parseVar, parseDeclaration]
 
 parseLit :: PU.Parser AT.Expr
 parseLit = do
@@ -19,14 +20,23 @@ parseLit = do
 
 parseVar :: PU.Parser AT.Expr
 parseVar = do
-  name <- PU.lexeme PU.identifier
+  name <- PU.identifier
   srcLoc <- parseSrcLoc
   env <- S.get
   case E.lookupVar name env of
     (Just t) -> return $ AT.Var srcLoc name t
     _ -> fail ""
 
+parseDeclaration :: PU.Parser AT.Expr
+parseDeclaration = do
+  name <- PU.lexeme PU.identifier
+  t <- PU.symbol ":" *> PT.parseType
+  value <- M.optional $ PU.symbol "=" *> parseExpr
+  srcLoc <- parseSrcLoc
+  S.modify (E.insertVar name t)
+  return $ AT.Declaration {AT.declLoc = srcLoc, AT.declName = name, AT.declType = t, AT.declInit = value}
+
 parseSrcLoc :: PU.Parser AT.SrcLoc
 parseSrcLoc = do
   (MP.SourcePos {MP.sourceName = _sourceName, MP.sourceLine = _sourceLine, MP.sourceColumn = _sourceColumn}) <- M.getSourcePos
-  return $ AT.SrcLoc {AT.srcLine = MP.unPos _sourceLine, AT.srcFile = _sourceName, AT.srcCol = MP.unPos _sourceColumn}
+  return $ AT.SrcLoc {AT.srcFile = _sourceName, AT.srcLine = MP.unPos _sourceLine, AT.srcCol = MP.unPos _sourceColumn}
