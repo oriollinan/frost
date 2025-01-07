3f38dd7654239a1df952596381f8b6ea18d35410
Renzo Maggiori <maggiorirenzo@gmail.com>
2025-01-07 11:32:42 +0100

    [fix] `Unary Operation` tests

--- test/Ast/Parser/UnaryOperationSpec.hs
+++ test/Ast/Parser/UnaryOperationSpec.hs
@@ -1,7 +1,7 @@
 module Ast.Parser.UnaryOperationSpec where
 
 import qualified Ast.Parser.Env as E
-import qualified Ast.Parser.UnaryOperation as AUO
+import qualified Ast.Parser.UnaryOperation as PO
 import qualified Ast.Types as AT
 import qualified Control.Monad.State as S
 import Data.Either (isLeft)
@@ -12,37 +12,37 @@ import qualified Text.Megaparsec.Char as MC
 spec :: Spec
 spec = do
   let initialEnv = E.emptyEnv
-  let parseWithEnv input =
-        fst $ S.runState (M.runParserT (AUO.parseUnaryOperation (MC.string "x")) "" input) initialEnv
+  let parseWithEnv input opType =
+        fst $ S.runState (M.runParserT (PO.parseUnaryOperation (MC.string "x") opType) "" input) initialEnv
 
   describe "parseUnaryOperation" $ do
     it "parses logical NOT" $ do
-      parseWithEnv "!x" `shouldBe` Right AT.Not
+      parseWithEnv "!x" PO.Pre `shouldBe` Right AT.Not
 
     it "parses 'not' logical unary operator" $ do
-      parseWithEnv "not x" `shouldBe` Right AT.Not
+      parseWithEnv "not x" PO.Pre `shouldBe` Right AT.Not
 
     it "parses bitwise NOT" $ do
-      parseWithEnv "~x" `shouldBe` Right AT.BitNot
+      parseWithEnv "~x" PO.Pre `shouldBe` Right AT.BitNot
 
     it "parses address-of operator" $ do
-      parseWithEnv "&x" `shouldBe` Right AT.AddrOf
+      parseWithEnv "&x" PO.Pre `shouldBe` Right AT.AddrOf
 
     it "parses pre-unary increment" $ do
-      parseWithEnv "++x" `shouldBe` Right AT.PreInc
+      parseWithEnv "++x" PO.Pre `shouldBe` Right AT.PreInc
 
     it "parses pre-unary decrement" $ do
-      parseWithEnv "--x" `shouldBe` Right AT.PreDec
+      parseWithEnv "--x" PO.Pre `shouldBe` Right AT.PreDec
 
     it "parses dereference operator" $ do
-      parseWithEnv "x." `shouldBe` Right AT.Deref
+      parseWithEnv "x." PO.Post `shouldBe` Right AT.Deref
 
     it "parses post-unary increment" $ do
-      parseWithEnv "x++" `shouldBe` Right AT.PostInc
+      parseWithEnv "x++" PO.Post `shouldBe` Right AT.PostInc
 
     it "parses post-unary decrement" $ do
-      parseWithEnv "x--" `shouldBe` Right AT.PostDec
+      parseWithEnv "x--" PO.Post `shouldBe` Right AT.PostDec
 
     it "returns error for invalid operator" $ do
-      let result = parseWithEnv "invalid"
+      let result = parseWithEnv "invalid" PO.Pre
       isLeft result `shouldBe` True
