efcce490a1f17d37691a65747d9eedeaea1b3ea5
Renzo Maggiori <maggiorirenzo@gmail.com>
2025-01-19 16:02:46 +0100

    [add] `Parser PreProcessor` haddock documentation comments

--- lib/Ast/Parser/PreProcessor.hs
+++ lib/Ast/Parser/PreProcessor.hs
@@ -7,6 +7,9 @@ import qualified Ast.Parser.Utils as PU
 import qualified Control.Monad.State as S
 import qualified Text.Megaparsec as M
 
+-- | Preprocesses a source file by handling imports, defines, and other preprocessing tasks.
+-- It recursively resolves imports and applies preprocessing rules to the source.
+-- Returns the fully preprocessed source code as a `String`.
 preprocess :: String -> PU.Parser String
 preprocess sourceFile = do
   S.modify $ PS.insertImport sourceFile
